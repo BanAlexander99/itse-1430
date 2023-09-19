a17d93cc1340065db206a6633222f4dc7dc11b85
Alexander Ban <alexander.ban@my.tccd.edu>
2023-09-19 16:45:17 -0500

    Story 2

--- labs/Lab1/AlexanderBan.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/AlexanderBan.AdventureGame.ConsoleHost/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿/*
+ * ITSE 1430
+ * Fall 2023
+ * Alexander Ban
+ * Lab One
+ */
+ReadIntroduction();
+
+
+
+//Functions
+
+void ReadIntroduction ()
+{
+    Console.WriteLine("Welcome to the ITSE 1430 Adventure Game, created by Alexander Ban! \nYou awaken at the center of a dark room, the air heavy with dust. \nThe room is quiet, and the only features you see are heavy oak doors adorning each wall.\n With no other option, you begin walking.");
+    return;
+}
