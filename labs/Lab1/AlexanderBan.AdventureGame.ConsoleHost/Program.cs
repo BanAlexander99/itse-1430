d91b3af2489d80939340f27e37a177eccdc81a27
Alexander Ban <alexander.ban@my.tccd.edu>
2023-09-19 17:04:03 -0500

    Story 3

--- labs/Lab1/AlexanderBan.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/AlexanderBan.AdventureGame.ConsoleHost/Program.cs
@@ -6,7 +6,26 @@
  */
 ReadIntroduction();
 
+var quit = false;
+string command;
 
+do
+{
+    switch (GetAction())
+    {
+        case "N": command = "N"; break;
+        case "S": command = "S"; break;
+        case "E": command = "E"; break;
+        case "W": command = "W"; break;
+        case "Q": command = "Q"; break;
+        //{
+        //    quit = true;
+        //    break;
+        //};
+        
+
+    };
+} while (!quit);
 
 //Functions
 
@@ -14,4 +33,26 @@ void ReadIntroduction ()
 {
     Console.WriteLine("Welcome to the ITSE 1430 Adventure Game, created by Alexander Ban! \nYou awaken at the center of a dark room, the air heavy with dust. \nThe room is quiet, and the only features you see are heavy oak doors adorning each wall.\n With no other option, you begin walking.");
     return;
+}
+string GetAction ()
+{
+    Console.WriteLine("----------");
+    Console.WriteLine("N) Move North");
+    Console.WriteLine("S) Move South");
+    Console.WriteLine("E) Move East");
+    Console.WriteLine("W) Move West");
+    Console.WriteLine("Q) Quit Game");
+
+    do
+    {
+        switch(Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.N: return "N";
+            case ConsoleKey.S: return "S";
+            case ConsoleKey.E: return "E";
+            case ConsoleKey.W: return "W";
+            case ConsoleKey.Q: return "Q";
+            default: Console.WriteLine("Command not recognized. Please try again."); break;
+        };
+    } while (true);
 }
