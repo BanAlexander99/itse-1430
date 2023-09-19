4448a2549804f15205f14fbcd5e9831a46a72972
Alexander Ban <alexander.ban@my.tccd.edu>
2023-09-19 18:29:54 -0500

    Story 7 and 8

--- labs/Lab1/AlexanderBan.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/AlexanderBan.AdventureGame.ConsoleHost/Program.cs
@@ -7,13 +7,16 @@
 ReadIntroduction();
 
 var quit = false;
-string command;
+//string command;
 int coord = 4;
 
 
 do
 {
+    ShowMap(coord);
     ReadRoom(coord);
+    if (GeneratePercentage() <=5)
+        SeeShadows();
     switch (GetAction())
     {
         case "N": coord = MovePlayer(coord, "N"); break;
@@ -172,22 +175,36 @@ static void ReadRoom ( int coordinate)
 {
     switch (coordinate)
     {
-        case 1: Console.WriteLine("Room One");  break;
-        case 2: Console.WriteLine("Room Two");  break; 
-        case 3: Console.WriteLine("Room Three");  break;
-        case 4: Console.WriteLine("Room Four");  break;
-        case 5: Console.WriteLine("Room Five");  break;
-        case 6: Console.WriteLine("Room Six");  break;
-        case 7: Console.WriteLine("Room Seven");  break;
-        case 8: Console.WriteLine("Room Eight");  break;
-        case 9: Console.WriteLine("Room Nine");  break;
+        case 1: Console.WriteLine("You feel an air of delight as you gaze at the bright ballroom before you. \nThe atmosphere is such that you can almost feel the mirth of past parties held in this grand arena.\n There are doors on the southern and eastern walls");  break;
+        case 2: Console.WriteLine("The stale smell of bread fills your lungs as you step into this quiet kitchen.\n Rats scurry around you, grabbing what morsels of food are left.\nThere are doors to the west, south, and east.");  break; 
+        case 3: Console.WriteLine("Rows of rusted swords and spears line the walls of this abandoned armory.\n The sight of so many tools of war fills you with a sense of dread, which you quickly shake off.\n There are exits on the west and south walls.");  break;
+        case 4: Console.WriteLine("What was once a grand entrance way is now littered with cobwebs and dust.\n Two suits of armor flank the eastern door, forever guarding this abandoned portal.\nOther than the eastern door, you can travel north and south from here.");  break;
+        case 5: Console.WriteLine("This room is dominated by two long dining tables, lined with luxurious satin tablecloths.\n You feel your empty stomach grumble as you imagine the feasts once held here.\n There is an exit in every direction.");  break;
+        case 6: Console.WriteLine("At last you can see open air once you enter this courtyard.\nA couple of lone trees are all that grow in this now abandoned garden.\nYou can exit through the north, south, or west.");  break;
+        case 7: Console.WriteLine("Not watching your step, you almost fall headfirst into the empty hole that once was a swimming pool. The silence in this room is almost deafening.\n You can leave by heading north or east.");  break;
+        case 8: Console.WriteLine("This smartly decorated room was designed to house the guests of the castle.\nSeveral beds line the walls, and there are some chairs near a dresser.\nThis room has doors located to the north, east, and west.");  break;
+        case 9: Console.WriteLine("The grand majesty of the master bedroom is quite a sight to behold.\nSilk sheets adorn a massive bed, alongside a walk in closet and personal washroom.\nYou can leave by going north or west.");  break;
     }
 }
 static void PlayError ()
 {
     Console.WriteLine("Cannot move in that direction");
 }
-
+static void ShowMap (int coordinate)
+{
+    Console.WriteLine("1 2 3");
+    Console.WriteLine("4 5 6");
+    Console.WriteLine("7 8 9");
+    Console.WriteLine($"You are in Room {coordinate}");
+}
+static int GeneratePercentage()
+{
+    return Random.Shared.Next(1, 101);
+}
+static void SeeShadows ()
+{
+    Console.WriteLine("As you open the door, a flicker of movement draws your eye.\nA shadowy figure exits the room through a different door silently.")
+}
 ///Room Design
 ///1 2 3
 ///4 5 6
