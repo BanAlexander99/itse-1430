b75f042ea436b61fed7f905bd2be4b9faaa45080
Alexander Ban <alexander.ban@my.tccd.edu>
2023-10-17 18:53:40 -0500

    Hopefully updated to thursday's lecture

--- classwork/MovieLibrary/WinHost/MainForm.Designer.cs
+++ classwork/MovieLibrary/WinHost/MainForm.Designer.cs
@@ -1,148 +1,101 @@
 ﻿namespace MovieLibrary.WinHost
 {
-    partial class MainForm
+    public partial class MainForm : Form
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose ( bool disposing )
+        public MainForm ()
         {
-            if (disposing && (components != null))
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing ( FormClosingEventArgs e )
+        {
+            if (_movie != null)
             {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+                if (!Confirm("Do you want to exit?", "Exit"))
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            };
+
+            base.OnFormClosing(e);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent ()
+        private void OnFileExit ( object sender, EventArgs e )
         {
-            this._mainMenu = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.moviesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this._mainMenu.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // _mainMenu
-            // 
-            this._mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-            this.moviesToolStripMenuItem,
-            this.helpToolStripMenuItem});
-            this._mainMenu.Location = new System.Drawing.Point(0, 0);
-            this._mainMenu.Name = "_mainMenu";
-            this._mainMenu.Size = new System.Drawing.Size(800, 24);
-            this._mainMenu.TabIndex = 0;
-            this._mainMenu.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.exitToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "&File";
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.OnFileExit);
-            // 
-            // moviesToolStripMenuItem
-            // 
-            this.moviesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.addToolStripMenuItem,
-            this.editToolStripMenuItem,
-            this.deleteToolStripMenuItem});
-            this.moviesToolStripMenuItem.Name = "moviesToolStripMenuItem";
-            this.moviesToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
-            this.moviesToolStripMenuItem.Text = "&Movies";
-            // 
-            // addToolStripMenuItem
-            // 
-            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
-            this.addToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Insert;
-            this.addToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
-            this.addToolStripMenuItem.Text = "&Add";
-            this.addToolStripMenuItem.Click += new System.EventHandler(this.OnAddMovie);
-            // 
-            // editToolStripMenuItem
-            // 
-            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.Insert)));
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
-            this.editToolStripMenuItem.Text = "&Edit";
-            this.editToolStripMenuItem.Click += new System.EventHandler(this.OnEditMovie);
-            // 
-            // deleteToolStripMenuItem
-            // 
-            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            this.deleteToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Delete;
-            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
-            this.deleteToolStripMenuItem.Text = "&Delete";
-            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.OnDeleteMovie);
-            // 
-            // helpToolStripMenuItem
-            // 
-            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.aboutToolStripMenuItem});
-            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.helpToolStripMenuItem.Text = "&Help";
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.aboutToolStripMenuItem.Text = "&About";
-            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.OnHelpAbout);
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this._mainMenu);
-            this.MainMenuStrip = this._mainMenu;
-            this.Name = "MainForm";
-            this.Text = "Movie Library";
-            this._mainMenu.ResumeLayout(false);
-            this._mainMenu.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            Close();
         }
 
-        #endregion
+        private void OnAddMovie ( object sender, EventArgs e )
+        {
+            var dlg = new MovieForm();
 
-        private MenuStrip _mainMenu;
-        private ToolStripMenuItem fileToolStripMenuItem;
-        private ToolStripMenuItem exitToolStripMenuItem;
-        private ToolStripMenuItem moviesToolStripMenuItem;
-        private ToolStripMenuItem addToolStripMenuItem;
-        private ToolStripMenuItem editToolStripMenuItem;
-        private ToolStripMenuItem deleteToolStripMenuItem;
-        private ToolStripMenuItem helpToolStripMenuItem;
-        private ToolStripMenuItem aboutToolStripMenuItem;
+            //ShowDialog - modal
+            //Show - modeless
+            //dlg.Show();
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            //TODO: Add movie to library
+            _movie = dlg.Movie;
+            RefreshMovies();
+        }
+
+        private void OnEditMovie ( object sender, EventArgs e )
+        {
+            var movie = GetSelectedMovie();
+            if (movie == null)
+                return;
+
+            var dlg = new MovieForm();
+            dlg.Movie = movie;
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            //TODO: Add movie to library
+            _movie = dlg.Movie;
+            RefreshMovies();
+        }
+
+        private void OnDeleteMovie ( object sender, EventArgs e )
+        {
+            var movie = GetSelectedMovie();
+            if (movie == null)
+                return;
+
+            if (!Confirm("Delete", $"Are you sure you want to delete '{movie.Title}'?"))
+                return;
+
+            //TODO: Delete movie
+            _movie = null;
+            RefreshMovies();
+        }
+
+        private void OnHelpAbout ( object sender, EventArgs e )
+        {
+            var about = new AboutBox();
+            about.ShowDialog();
+        }
+
+        private bool Confirm ( string title, string message )
+        {
+            return MessageBox.Show(message, title, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes;
+        }
+
+        private Movie GetSelectedMovie ()
+        {
+            return _movie;
+        }
+
+        private void RefreshMovies ()
+        {
+            _lstMovies.DataSource = null;
+
+            //HACK: Fix this
+            if (_movie != null)
+                _lstMovies.DataSource = new[] { _movie };
+        }
+
+        private Movie _movie;
     }
 }
